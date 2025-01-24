9ecddbda835e050ae49a0c305364bfd92b30050c
Greg <gregcourt10@gmail.com>
2025-01-24 11:15:21 +0000

    added RGs

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,23 @@
 variable "container_name" {
   description = "The name of the Azure Storage container to store Terraform state file"
 }
+
+variable "prim_loc" {
+  description = "The primary location for the Azure resources"
+  default     = "uksouth"
+}
+
+variable "prim_loc_short" {
+  description = "The short name of the primary location for the Azure resources"
+  default     = "uks"
+}
+
+variable "sec_loc" {
+  description = "The secondary location for the Azure resources"
+  default     = "ukwest"
+}
+
+variable "sec_loc_short" {
+  description = "The short name of the secondary location for the Azure resources"
+  default     = "ukw"
+}
