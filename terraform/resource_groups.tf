12a10995201a522e7f9a87f2ac1e0f8a683792a5
Greg Court <83063651+Greg-Court@users.noreply.github.com>
2025-02-01 11:10:25 +0000

    Dev (#27)
    * testcomment added
    
    * test
    
    * removed execute from readme
    
    * updated dev branch to allow destroy
    
    * updates
    
    * skipping checkov CKV_GHA_7
    
    * setting cd-dev to trigger from CI
    
    * Reverted CD-Prod.yml to previous version
    
    * updates
    
    * updates
    
    * updates
    
    * set CI to on push to dev, but PR to dev or main
    
    * fixed mixed up pippelines
    
    * pipeline updates
    
    * added tf var env
    
    * added TF VAR for env for destroy piplines
    
    * removed CI on PR to main
    
    * updated names
    
    * fixed CD-dev torun from dev branch
    
    * updated steps names
    
    * pipeline updates
    
    * comment
    
    * testing
    
    * updated to checkout v4
    
    * renaming RGs
    
    * updated RG names v2
    
    * renamed RGs v3
    
    * test
    
    * updated CD-prod.yml
    
    * test
    
    * updated
    
    * CD-prod improvements
    
    * RG update!
    
    * separated plan & apply
    
    * updated upload/download artifact to v4"
    "
    
    * fixed missing "evnironment" on dev
    
    * test
    
    * updates
    
    * major updates to pipelines

--- terraform/resource_groups.tf
+++ terraform/resource_groups.tf
@@ -1,15 +1,15 @@
 resource "azurerm_resource_group" "web" {
-  name     = "rg-web-bb-updated-app-${var.env}-${var.prim_loc_short}-01"
+  name     = "rg-web-bb-TEST-app-${var.env}-${var.prim_loc_short}-01"
   location = var.prim_loc
 }
 
 resource "azurerm_resource_group" "sql" {
-  name     = "rg-sql-bb-updated-app-${var.env}-${var.prim_loc_short}-01"
+  name     = "rg-sql-bb-TEST-app-${var.env}-${var.prim_loc_short}-01"
   location = var.prim_loc
 }
 
 resource "azurerm_resource_group" "net" {
-  name     = "rg-net-bb-updated-app-${var.env}-${var.prim_loc_short}-01"
+  name     = "rg-net-bb-TEST-app-${var.env}-${var.prim_loc_short}-01"
   location = var.prim_loc
 }
 
