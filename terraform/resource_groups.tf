3312e1c5ae7d20bbb4e0c315d01d14acfd7ce87b
Greg <gregcourt10@gmail.com>
2025-02-01 11:49:40 +0000

    test

--- terraform/resource_groups.tf
+++ terraform/resource_groups.tf
@@ -1,17 +1,18 @@
+locals {
+  pipeline_testing = "Version1"
+}
+
 resource "azurerm_resource_group" "web" {
-  name     = "rg-web-bb-TEST-app-${var.env}-${var.prim_loc_short}-01"
+  name     = "rg-web-${local.pipeline_testing}-app-${var.env}-${var.prim_loc_short}-01"
   location = var.prim_loc
 }
 
 resource "azurerm_resource_group" "sql" {
-  name     = "rg-sql-bb-TEST-app-${var.env}-${var.prim_loc_short}-01"
+  name     = "rg-sql-${local.pipeline_testing}-app-${var.env}-${var.prim_loc_short}-01"
   location = var.prim_loc
 }
 
 resource "azurerm_resource_group" "net" {
-  name     = "rg-net-bb-TEST-app-${var.env}-${var.prim_loc_short}-01"
+  name     = "rg-net-${local.pipeline_testing}-app-${var.env}-${var.prim_loc_short}-01"
   location = var.prim_loc
 }
-
-
-# test comment
