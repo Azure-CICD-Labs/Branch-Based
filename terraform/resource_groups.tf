3a5e69f468f36633cfac5c648bbcf83d3db2af37
Greg Court <83063651+Greg-Court@users.noreply.github.com>
2025-02-01 08:52:08 +0000

    Dev (#19)
    * testcomment added
    
    * test
    
    * removed execute from readme
    
    * updated dev branch to allow destroy
    
    * updates
    
    * skipping checkov CKV_GHA_7
    
    * setting cd-dev to trigger from CI
    
    * Reverted CD-Prod.yml to previous version
    
    * updates
    
    * updates
    
    * updates
    
    * set CI to on push to dev, but PR to dev or main
    
    * fixed mixed up pippelines
    
    * pipeline updates
    
    * added tf var env
    
    * added TF VAR for env for destroy piplines
    
    * removed CI on PR to main
    
    * updated names
    
    * fixed CD-dev torun from dev branch
    
    * updated steps names
    
    * pipeline updates
    
    * comment
    
    * testing
    
    * updated to checkout v4
    
    * renaming RGs
    
    * updated RG names v2
    
    * renamed RGs v3
    
    * test
    
    * updated CD-prod.yml
    
    * test
    
    * updated
    
    * CD-prod improvements

--- terraform/resource_groups.tf
+++ terraform/resource_groups.tf
@@ -1,15 +1,15 @@
 resource "azurerm_resource_group" "web" {
-  name     = "rg-web-updated-bb-app-${var.env}-${var.prim_loc_short}-01"
+  name     = "rg-web-bb-app-${var.env}-${var.prim_loc_short}-01"
   location = var.prim_loc
 }
 
 resource "azurerm_resource_group" "sql" {
-  name     = "rg-sql-updated-bb-app-${var.env}-${var.prim_loc_short}-01"
+  name     = "rg-sql-bb-app-${var.env}-${var.prim_loc_short}-01"
   location = var.prim_loc
 }
 
 resource "azurerm_resource_group" "net" {
-  name     = "rg-net-updated-bb-app-${var.env}-${var.prim_loc_short}-01"
+  name     = "rg-net-bb-app-${var.env}-${var.prim_loc_short}-01"
   location = var.prim_loc
 }
 
