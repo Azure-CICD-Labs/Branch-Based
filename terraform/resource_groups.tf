357799dc7184a102da8a6768227d60be978063ce
Greg <gregcourt10@gmail.com>
2025-02-02 09:05:01 +0000

    test123

--- terraform/resource_groups.tf
+++ terraform/resource_groups.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "this" {
-  name     = "rg-test9000-${var.env}-${var.location_short}-01"
+  name     = "rg-test123-${var.env}-${var.location_short}-01"
   location = var.location
 }
 
