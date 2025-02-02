3f1d5f70567d34e15b637a13ec79c62cd0806716
Greg <gregcourt10@gmail.com>
2025-02-02 10:14:54 +0000

    12345

--- terraform/resource_groups.tf
+++ terraform/resource_groups.tf
@@ -1,10 +1,6 @@
 resource "azurerm_resource_group" "this" {
-<<<<<<< HEAD
-  name     = "rg-123-${var.env}-${var.location_short}-01"
-=======
-  name     = "rg-e2e-${var.env}-${var.location_short}-01"
->>>>>>> 9204f3c (end-to-end testing)
+  name     = "rg-12345-${var.env}-${var.location_short}-01"
   location = var.location
 }
 
-# 123
+# 12345
