0a9163e8de14cd5268b624efac230654fd306238
Greg <gregcourt10@gmail.com>
2025-02-02 14:59:38 +0000

    bb

--- terraform/resource_groups.tf
+++ terraform/resource_groups.tf
@@ -1,6 +1,4 @@
 resource "azurerm_resource_group" "this" {
-  name     = "rg-9000-${var.env}-${var.location_short}-01"
+  name     = "rg-bb-${var.env}-${var.location_short}-01"
   location = var.location
 }
-
-# 9000
